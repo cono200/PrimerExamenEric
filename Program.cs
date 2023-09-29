652fa897920e9f4e882707f786ee264adb3de537
Thomas Orozco <thomasorozcog@gmail.com>
2023-09-29 09:05:23 -0700

    examen

--- Program.cs
+++ Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            otraClase otraClase1 = new otraClase();
+            //CURP
+            /*
+            Console.WriteLine("Esribe la curp");
+            string curp= Convert.ToString(Console.ReadLine());
+
+            otraClase1.CurpPersona(curp);
+            
+            
+            */
+
+            //INTERVALO DE NUMEROS
+            // QUE NO JALA DEL 25 AL 20 PERO SI ALREVEZ :(
+            /*
+            Console.WriteLine("Escribe el primer numero");
+            int numero1=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Escribe el segundo");
+            int numero2=Convert.ToInt32(Console.ReadLine());
+
+            otraClase1.IntervaloNumeros(numero1, numero2);
+
+            */
+
+            //PROBLEMA RECURSIVO
+            
+            Console.WriteLine("Escribe el numero");
+            int numero2= Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(otraClase1.TablasMultiplicar(numero2));
+            
         }
+
     }
 }
---
+++ otraClase.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenEric_SLE
+{
+    internal class otraClase
+    {
+        //curp[curp.Length/2]
+
+        /*
+        public void CurpPersona(string curp)
+        {
+            if (  curp.Length > 18)
+            {
+                Console.WriteLine("No puede existir esa curp");
+            }
+
+            //DateOnly fecha = DateOnly.ParseExact(year, "yy/MM/dd", CultureInfo.InvariantCulture);
+
+            else if (curp[curp.Length /2] = "F")
+            {
+
+                Console.WriteLine("Mujer");
+            }
+        */
+
+        //INTERVALO DE VALORES
+
+        public void IntervaloNumeros(int numero1, int numero2)
+        {
+            if (numero1 == numero2)
+            {
+                Console.WriteLine("El intervalo de valores son iguales");
+            }
+          else  if (numero1 < numero2)
+            {
+                for (int i = numero1; i <= numero2; i++)
+                {
+                    numero1 = +i;
+                    Console.WriteLine("la secuencia de los números es " + numero1);
+                }
+            }
+            //NO JALA ALREVEZ
+            else if (numero1 > numero2) 
+            {
+                for (int i = numero2; i <= numero1; i++)
+                {
+                    numero2 = +i;
+                    Console.WriteLine("la secuencia de los números es " + numero2);
+                }
+            }
+
+        }
+
+
+        public int TablasMultiplicar(int numero2)
+        {
+            
+
+            if (numero2 ==0)
+            {
+                return numero2;
+            }
+
+            return  numero2  * TablasMultiplicar(numero2 -1);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
